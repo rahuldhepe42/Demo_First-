8a2620845bc6739e3ec1e45fd6ffc3a290512bff
Rahul Dhepe <rahuldhepe42@gmail.com>
2024-03-09 06:13:15 -0800

    Project is added to github

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactusComponent } from './contactus/contactus.component';
+
+const routes: Routes = [
+  {path:"home", component:HomeComponent},
+  {path:"about", component:AboutComponent},
+  {path:"contactus", component:ContactusComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { } export const 
+RoutingComponent=[HomeComponent,AboutComponent,ContactusComponent];
